0d08c41aa748ae7f1ae15bc0de2e2d1b39ced4a8
Akhmedzyanov Rustam <astonvista@gmail.com>
2025-06-16 12:21:12 +0300

    Controller tests added

--- src/main/java/com/example/user_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/user_service/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.user_service.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +11,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<Map<String, String>> handleUserNotFound(UserNotFoundException e) {
+        log.info("пользователь не найден");
         return new ResponseEntity<>(Map.of(e.getField(), e.getMessage()), HttpStatus.NOT_FOUND);
     }
 
@@ -23,17 +26,20 @@ public class GlobalExceptionHandler {
         Map<String, String> errors = new HashMap<>();
         e.getFieldErrors().forEach(
                 fieldError -> errors.put(fieldError.getField(), fieldError.getDefaultMessage()));
+        log.warn("Введены некорректные данные");
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<Map<String, String>> handleDataIntegrityViolation(DataIntegrityViolationException e) {
+        log.warn("Введенный e-mail уже существует");
         return new ResponseEntity<>(Map.of("Введенный e-mail уже существует: ", e.getMessage()),
                 HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, String>> handleAll(Exception e) {
+        log.error("Неизвестная ошибка");
         return new ResponseEntity<>(Map.of("error: ", e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 }
--- src/test/java/com/example/user_service/UserServiceApplicationTests.java
+++ src/test/java/com/example/user_service/UserServiceApplicationTests.java
@@ -1,13 +1,159 @@
 package com.example.user_service;
 
+import com.example.user_service.controller.UserController;
+import com.example.user_service.dto.UserDto;
+import com.example.user_service.exception.UserNotFoundException;
+import com.example.user_service.service.UserService;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@AutoConfigureWebMvc
+@WebMvcTest(UserController.class)
 class UserServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    MockMvc mockMvc;
 
+    @Autowired
+    UserService userService;
+
+    @TestConfiguration
+    static class TestConfig {
+
+        @Bean
+        UserService userService() {
+            return Mockito.mock(UserService.class);
+        }
+    }
+
+    @Test
+    void getAll_returnsTwoElementList() throws Exception {
+        List<UserDto> list = Arrays.asList(
+                new UserDto(1L, "TestOne", "test1@test.com", 36, LocalDateTime.now()),
+                new UserDto(2L, "TestTwo", "test2@test.com", 37, LocalDateTime.now()));
+
+        when(userService.getAll()).thenReturn(list);
+
+        mockMvc.perform(get("/api/users"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("TestOne"))
+                .andExpect(jsonPath("$[0].email").value("test1@test.com"))
+                .andExpect(jsonPath("$[0].age").value(36))
+                .andExpect(jsonPath("$[1].name").value("TestTwo"))
+                .andExpect(jsonPath("$[1].email").value("test2@test.com"))
+                .andExpect(jsonPath("$[1].age").value(37));
+    }
+
+    @Test
+    void getById_returnsUserDto() throws Exception {
+        UserDto user = new UserDto(1L, "TestOne", "test1@test.com", 36, LocalDateTime.now());
+
+        when(userService.getById(1L)).thenReturn(user);
+
+        mockMvc.perform(get("/api/users/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("TestOne"))
+                .andExpect(jsonPath("$.email").value("test1@test.com"))
+                .andExpect(jsonPath("$.age").value(36));
+    }
+
+    @Test
+    void getById_shouldReturn404WhenUserNotFound() throws Exception {
+        when(userService.getById(99L)).thenThrow(new UserNotFoundException("Пользователь с ID 99 не найден"));
+
+        mockMvc.perform(get("/api/users/99")).andExpect(status().isNotFound());
+    }
+
+    @Test
+    void create_shouldCreateUser() throws Exception {
+        UserDto user = new UserDto(1L, "TestOne", "test1@test.com", 36, LocalDateTime.now());
+        when(userService.create(any())).thenReturn(user);
+
+        mockMvc.perform(post("/api/users").contentType(MediaType.APPLICATION_JSON).content("""
+                        {
+                          "name": "TestOne",
+                          "email": "test1@test.com",
+                          "age": 36
+                        }
+                        """))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value("TestOne"))
+                .andExpect(jsonPath("$.email").value("test1@test.com"))
+                .andExpect(jsonPath("$.age").value(36));
+    }
+
+    @Test
+    void create_shouldReturn400WhenInvalidUser() throws Exception {
+        mockMvc.perform(post("/api/users").contentType(MediaType.APPLICATION_JSON).content("""
+                        {
+                          "name": "",
+                          "email": "wrong",
+                          "age": 10
+                        }
+                        """))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void update_shouldUpdateUser() throws Exception {
+        UserDto user = new UserDto(1L, "TestOne", "test1@test.com", 36, LocalDateTime.now());
+        when(userService.update(eq(1L), any())).thenReturn(user);
+
+        mockMvc.perform(put("/api/users/1").contentType(MediaType.APPLICATION_JSON).content("""
+                        {
+                          "name": "TestOne",
+                          "email": "test1@test.com",
+                          "age": 36
+                        }
+                        """))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("TestOne"))
+                .andExpect(jsonPath("$.email").value("test1@test.com"))
+                .andExpect(jsonPath("$.age").value(36));
+    }
+
+    @Test
+    void update_shouldReturn404OnNonExistentUser() throws Exception {
+        when(userService.update(eq(99L), any())).thenThrow(new UserNotFoundException("Пользователь не найден"));
+
+        mockMvc.perform(put("/api/users/99").contentType(MediaType.APPLICATION_JSON).content("""
+                        {
+                          "name": "TestOne",
+                          "email": "test1@test.com",
+                          "age": 36
+                        }
+                        """))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void delete_shouldDeleteUser() throws Exception {
+        mockMvc.perform(delete("/api/users/1"))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void shouldReturn404OnDeleteNonExistentUser() throws Exception {
+        doThrow(new UserNotFoundException("Пользователь c ID 99 не найден")).when(userService).delete(99L);
+
+        mockMvc.perform(delete("/api/users/99"))
+                .andExpect(status().isNotFound());
+    }
 }
